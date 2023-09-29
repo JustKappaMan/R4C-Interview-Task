2196e9d77039443c8145f633f782433b28a57471
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-09-29 13:38:53 +0500

    Add favicons

--- R4C/urls.py
+++ R4C/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.templatetags.static import static
 
 from home.views import index_view
 
 
 urlpatterns = [
+    path("", index_view),
     path("admin/", admin.site.urls),
     path("robots/", include("robots.urls")),
     path("orders/", include("orders.urls")),
-    path("", index_view),
+    path("favicon.ico", RedirectView.as_view(url=static("images/icons/favicon.ico"))),
 ]
