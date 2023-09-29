44b6c553474b05136b6af42b8b9b908b9d9e9200
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-09-29 19:22:23 +0500

    Move signal to `signals.py`

---
+++ orders/signals.py
@@ -0,0 +1,24 @@
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.db.models.signals import post_save
+
+from orders.models import Order
+from robots.models import Robot
+
+
+@receiver(post_save, sender=Robot)
+def notify_customers_robot_available(sender, instance, **kwargs) -> None:
+    try:
+        orders = Order.objects.filter(robot_serial=instance.serial)
+    except Order.DoesNotExist:
+        pass
+    else:
+        email_subject = "Робот доступен к покупке!"
+        email_message = (
+            "Здравствуйте!\n\n"
+            f"Недавно Вы интересовались нашим роботом модели {instance.model}, версии {instance.version}.\n\n"
+            "Этот робот теперь в наличии. Если Вам подходит этот вариант - пожалуйста, свяжитесь с нами."
+        )
+        for order in orders:
+            send_mail(email_subject, email_message, "info@robocomplex.com", (order.customer.email,))
+            order.delete()
--- orders/utils.py
+++ orders/utils.py
@@ -1,10 +1,6 @@
 from django.http import HttpRequest
-from django.dispatch import receiver
-from django.core.mail import send_mail
-from django.db.models.signals import post_save
 
 from robots.models import Robot
-from orders.models import Order
 from customers.models import Customer
 
 
@@ -33,21 +29,3 @@ def validate_new_order_request(request: HttpRequest) -> dict[str, str] | None:
         raise ValueError("email is invalid")
 
     return {"serial": serial, "email": email}
-
-
-@receiver(post_save, sender=Robot)
-def notify_customers_robot_available(sender, instance, **kwargs):
-    try:
-        orders = Order.objects.filter(robot_serial=instance.serial)
-    except Order.DoesNotExist:
-        pass
-    else:
-        email_subject = "Робот доступен к покупке!"
-        email_message = (
-            "Здравствуйте!\n\n"
-            f"Недавно Вы интересовались нашим роботом модели {instance.model}, версии {instance.version}.\n\n"
-            "Этот робот теперь в наличии. Если Вам подходит этот вариант - пожалуйста, свяжитесь с нами."
-        )
-        for order in orders:
-            send_mail(email_subject, email_message, "from@example.com", (order.customer.email,))
-            order.delete()
